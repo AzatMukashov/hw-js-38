6b2412fcff9e1690836aa3bb3e4e608f11ea44a6
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-11 18:26:19 +0600

    Внутри компонента создал функции: dispatch, tasks: Получает список задач из состояния Redux, loading: Получает статус загрузки из состояния Redux. useEffect для вызова fetchTasks при монтировании компонента. Разметка: Отображение текста "Loading..." при загрузке данных. Отображение списка задач с помощью компонента для каждой задачи, если загрузка завершена.

---
+++ src/components/TasksList.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../app/store.ts';
+import { fetchTasks } from '../app/tasksSlice.ts';
+import TaskItem from './TaskItem.tsx';
+
+const TasksList: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const tasks = useSelector((state: RootState) => state.tasks.tasks);
+  const loading = useSelector((state: RootState) => state.tasks.loading);
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <TaskItem key={task.id} task={task}/>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default TasksList;
