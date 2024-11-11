1f085ddbbac6c6270dccdc8e0ebeed315e56dc2a
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-11 18:14:12 +0600

    Создал интерфейс для типизации пропсов компонента, принимающего объект task. Внутри компонента созданы функции: handleCheckboxChange вызывает updateTaskStatus для обновления статуса задачи при изменении чекбокса. handleDelete вызывает deleteTask для удаления задачи при нажатии кнопки Delete. В разметке: чекбокс для пометки выполнения задачи. Отображение заголовка задачи. Кнопка для удаления задачи.

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Task } from '../types';
+import { useDispatch } from 'react-redux';
+import { deleteTask, updateTaskStatus } from '../app/tasksSlice.ts';
+import { AppDispatch } from '../app/store.ts';
+
+interface TaskItemProps {
+  task: Task;
+}
+
+const TaskItem: React.FC<TaskItemProps> = ({task}) => {
+  const dispatch: AppDispatch = useDispatch();
+  const handleCheckboxChange = () => {
+    dispatch(updateTaskStatus({...task, completed: !task.completed}));
+  };
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id));
+  }
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={handleCheckboxChange}
+      />
+      {task.title}
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
+
+export default TaskItem;
